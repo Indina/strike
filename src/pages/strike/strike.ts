89306ffdc3900f3e54a90c04eb8f4b75e6137252
Indina Smith <Indina Smith>
2017-08-01 10:43:17 +0200

    test for indina smith

--- src/pages/strike/strike.ts
+++ src/pages/strike/strike.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
-import {StrikeItem} from '../../model/strikeItem';
+//import {StrikeItem} from '../../model/strikeItem';
+import {SendPage} from '../send/send';
+
 
 @Component({
   selector: 'page-strike',
@@ -16,6 +18,11 @@ export class StrikePage {
       this.strike = afConnection.list("/strikeItems");
   }
 
+
+send(){
+this.navCtrl.push(SendPage);
+
+}
 }
 
 
